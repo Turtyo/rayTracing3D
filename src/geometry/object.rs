d194e0eae8fe7983de80fe1578cbfd4805ae0270
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-03-12 20:43:52 +0100

    Rewrite the sphere creation with updated vector struct

--- src/geometry/object.rs
+++ src/geometry/object.rs
@@ -1,14 +1,19 @@
 use super::point::Point;
-use super::vector::{Vector, BaseVector};
+use super::vector::Vector;
 
+#[derive(Debug, Clone, Copy)]
 pub struct Sphere {
     pub center: Point,
     pub radius: f64,
 }
 
 impl Sphere {
-    fn new_from_points(center: &Point, outer: &Point) -> Self {
-        let radius = BaseVector::norme(&BaseVector::new_from_points(center, outer));
-        Sphere {center : *center, radius}
+    pub fn new_from_points(center: &Point, outer: &Point) -> Self {
+        let Point { x, y, z } = outer - center;
+        let radius = Vector::norme(x, y, z);
+        Sphere {
+            center: *center,
+            radius,
+        }
     }
 }
