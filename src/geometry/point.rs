96fedd520eabc558b22bd87b29033669fbad933a
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-03-12 20:38:44 +0100

    Change point due to vector changes

--- src/geometry/point.rs
+++ src/geometry/point.rs
@@ -1,12 +1,19 @@
-use std::ops::Sub;
-use std::ops::Add;
-use crate::geometry::vector::{Vector, BaseVector, UnitVector};
+use crate::geometry::vector::Vector;
+use std::cmp::PartialEq;
+use std::ops::{Add, Sub};
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Point {
     pub x: f64,
     pub y: f64,
     pub z: f64,
+}
+
+impl Point {
+    pub fn distance(&self, other: &Point) -> f64 {
+        let Point { x, y, z } = self - other;
+        Vector::norme(x, y, z)
+    }
 }
 
 impl Sub for &Point {
@@ -29,19 +36,19 @@ impl Add for &Point {
     }
 }
 
-impl Add<&BaseVector> for &Point {
+impl Add<&Vector> for &Point {
     type Output = Point;
-    fn add(self, rhs: &BaseVector) -> Self::Output {
+    fn add(self, rhs: &Vector) -> Self::Output {
         let mut new_point = *self;
-        new_point.x += rhs.x;
-        new_point.y += rhs.y;
-        new_point.z += rhs.z;
+        new_point.x += rhs.norme * rhs.x();
+        new_point.y += rhs.norme * rhs.y();
+        new_point.z += rhs.norme * rhs.z();
 
         new_point
     }
 }
 
-impl Add<&Point> for &BaseVector {
+impl Add<&Point> for &Vector {
     type Output = Point;
     fn add(self, rhs: &Point) -> Self::Output {
         rhs + self
