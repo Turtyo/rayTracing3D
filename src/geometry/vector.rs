4d75ccf9b0499f2ff4551de15ea5bcc72b655b56
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-03-12 20:32:07 +0100

    Rewrite vector

--- src/geometry/vector.rs
+++ src/geometry/vector.rs
@@ -1,118 +1,110 @@
-use std::ops::Sub;
-use std::ops::Add;
-use std::ops::Mul;
-use std::ops::Div;
 use crate::geometry::point::Point;
-
-pub trait Vector {
-    fn x(&self) -> f64;
-    fn y(&self) -> f64;
-    fn z(&self) -> f64;
-
-    // ! not good, can't return any concrete type when using impl Vector
-    // fn new_from_coordinates(x: f64, y:f64, z:f64) -> impl Vector;
-    // fn new_from_points(origin: &Point, destination: &Point) -> impl Vector;
-
-    fn scalar_product(vector_1: &impl Vector, vector_2: &impl Vector) -> f64 {
-        vector_1.x() * vector_2.x() + vector_1.y() * vector_2.y() + vector_1.z() * vector_2.z()
-    }
-
-    fn norme(vector: &impl Vector) -> f64 {
-        Self::scalar_product(vector, vector).sqrt()
-    }
-}
+use std::ops::Add;
+use std::ops::Div;
+use std::ops::Mul;
+use std::ops::Sub;
 
 #[derive(Clone, Copy)]
-pub struct BaseVector {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
+pub struct Vector {
+    pub norme: f64,
+    pub direction: UnitVector,
 }
 
-impl BaseVector {
+impl Vector {
+    pub fn x(&self) -> f64 {
+        self.direction.x
+    }
+
+    pub fn y(&self) -> f64 {
+        self.direction.y
+    }
+
+    pub fn z(&self) -> f64 {
+        self.direction.z
+    }
+
     pub fn new_from_points(origin: &Point, destination: &Point) -> Self {
         {
             let Point { x, y, z } = destination - origin;
-            BaseVector { x, y, z }
+            Self::new_from_coordinates(x, y, z)
         }
     }
 
-    pub fn new_from_coordinates(x: f64, y:f64, z:f64) -> Self {
-        BaseVector{x, y, z}
+    pub fn new_from_coordinates(x: f64, y: f64, z: f64) -> Self {
+        let norme = Self::norme(x, y, z);
+        let direction = Self::normalize(x, y, z, norme);
+        Vector { norme, direction }
+    }
+
+    pub fn scalar_product(vector_1: &Vector, vector_2: &Vector) -> f64 {
+        vector_1.norme * vector_2.norme * vector_1.direction.scalar_product(&vector_2.direction)
+    }
+
+    pub fn norme(x: f64, y: f64, z: f64) -> f64 {
+        (x * x + y * y + z * z).sqrt()
+    }
+
+    fn normalize(x: f64, y: f64, z: f64, norme: f64) -> UnitVector {
+        let mut unit_vector = UnitVector { x, y, z };
+        // ! need to check norme != 0 and maybe return a result ?
+        unit_vector.x /= norme;
+        unit_vector.y /= norme;
+        unit_vector.z /= norme;
+
+        // ? is vector discarded here
+
+        unit_vector
     }
 }
 
-impl Vector for BaseVector {
-    fn x(&self) -> f64 {
-        self.x
-    }
-    fn y(&self) -> f64 {
-        self.y
-    }
-    fn z(&self) -> f64 {
-        self.z
-    }
-}
-
-// impl Add for &impl Vector {
-//     type Output = impl Vector;
-//     fn add(self, rhs: Self) -> Self::Output {
-        
-//     }
-// }
-
-impl Add for &BaseVector {
-    type Output = BaseVector;
+impl Add for &Vector {
+    type Output = Vector;
     fn add(self, rhs: Self) -> Self::Output {
-        let mut new_vector = *self;
+        let new_vector = *self;
 
-        new_vector.x += rhs.x;
-        new_vector.y += rhs.y;
-        new_vector.z += rhs.z;
+        let x = new_vector.norme * new_vector.x() + rhs.norme * rhs.x();
+        let y = new_vector.norme * new_vector.y() + rhs.norme * rhs.y();
+        let z = new_vector.norme * new_vector.z() + rhs.norme * rhs.z();
 
-        new_vector
+        Vector::new_from_coordinates(x, y, z)
     }
 }
 
-
-// ? how to implement mul for all &Vector
-// impl<T: Vector> Mul<f64> for &T 
-impl Mul<f64> for &BaseVector {
-    type Output = BaseVector;
+impl Mul<f64> for &Vector {
+    type Output = Vector;
     fn mul(self, rhs: f64) -> Self::Output {
         let mut mult_vec = *self;
-        mult_vec.x *= rhs;
-        mult_vec.y *= rhs;
-        mult_vec.z *= rhs;
+
+        mult_vec.norme *= rhs;
 
         mult_vec
-
     }
 }
 
-impl Mul<&BaseVector> for f64 {
-    type Output = BaseVector;
-    fn mul(self, rhs: &BaseVector) -> Self::Output {
+impl Mul<&Vector> for f64 {
+    type Output = Vector;
+    fn mul(self, rhs: &Vector) -> Self::Output {
         rhs * self
     }
 }
 
-impl Sub for &BaseVector {
-    type Output = BaseVector;
+impl Sub for &Vector {
+    type Output = Vector;
     fn sub(self, rhs: Self) -> Self::Output {
-        let neg_vec = -1.* rhs;
+        let neg_vec = -1. * rhs;
         // ? is there a way to oneline this with a closure maybe
         self + &neg_vec
     }
 }
 
-impl Div<f64> for &BaseVector {
-    type Output = BaseVector;
+impl Div<f64> for &Vector {
+    type Output = Vector;
     fn div(self, rhs: f64) -> Self::Output {
-        (1./rhs) * self 
+        (1. / rhs) * self
     }
 }
 
+// ! should be used in a way that this always has a norme of 1
 #[derive(Clone, Copy)]
 pub struct UnitVector {
     x: f64,
@@ -121,80 +113,51 @@ pub struct UnitVector {
 }
 
 impl UnitVector {
-    // ? how to make this more compact by merging code with BaseVector
+    pub fn scalar_product(&self, other: &UnitVector) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
     pub fn new_from_points(origin: &Point, destination: &Point) -> Self {
-        let Point { x, y, z } = destination - origin;
-        UnitVector::new_from_coordinates(x, y, z)
+        {
+            let Point { x, y, z } = destination - origin;
+            Self::new_from_coordinates(x, y, z)
+        }
     }
 
-    pub fn new_from_coordinates(x: f64, y:f64, z:f64) -> Self {
-        let vector = BaseVector { x, y, z };
-        let norme = Self::norme(&vector);
-        // ! need to check norme != 0 and maybe return a result ?
-        let mut unit_vector = Self {
-            x: 0.,
-            y: 0.,
-            z: 0.,
-        };
-        unit_vector.x = x / norme;
-        unit_vector.y = y / norme;
-        unit_vector.z = z / norme;
-
-        // ? is vector discarded here
-
-        unit_vector
+    pub fn new_from_coordinates(x: f64, y: f64, z: f64) -> Self {
+        let norme = Vector::norme(x, y, z);
+        Vector::normalize(x, y, z, norme)
     }
 
-    // ? how to make it return a UnitVector when new_from_coordinates returns a impl Vector
-
-    fn normalize(vector: &BaseVector) -> impl Vector {
-        let BaseVector{x,y,z} = *vector;
-        UnitVector::new_from_coordinates(x, y, z)
+    pub fn to_vector(&self) -> Vector {
+        Vector {
+            norme: 1.,
+            direction: *self,
+        }
     }
-}
-
-impl Vector for UnitVector {
-    fn x(&self) -> f64 {
-        self.x
-    }
-    fn y(&self) -> f64 {
-        self.y
-    }
-    fn z(&self) -> f64 {
-        self.z
-    }
-    // ! this is the same code as BaseVector, but we can't have a field in a trait
-
 }
 
 impl Mul<f64> for &UnitVector {
-    type Output = BaseVector;
+    type Output = Vector;
     fn mul(self, rhs: f64) -> Self::Output {
-        let UnitVector{mut x, mut y, mut z} = *self;
-        x *= rhs;
-        y *= rhs;
-        z *= rhs;
-
-        BaseVector{x, y, z}
-
+        Vector {
+            norme: rhs,
+            direction: *self,
+        }
     }
 }
 
 impl Mul<&UnitVector> for f64 {
-    type Output = BaseVector;
+    type Output = Vector;
     fn mul(self, rhs: &UnitVector) -> Self::Output {
         rhs * self
     }
 }
 
-impl Add<&BaseVector> for &UnitVector {
-    type Output = BaseVector;
-    fn add(self, rhs: &BaseVector) -> Self::Output {
-        let mut new_vec = *rhs;
-        new_vec.x += self.x;
-        new_vec.y += self.y;
-        new_vec.z += self.z;
-
-        new_vec
+impl Add<&Vector> for &UnitVector {
+    type Output = Vector;
+    fn add(self, rhs: &Vector) -> Self::Output {
+        // convert the unit vector to a Vector
+        &self.to_vector() + rhs
     }
 }
