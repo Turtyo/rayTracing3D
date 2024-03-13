f23c75d4526caac9204cc16c7a5fa2050cdd0505
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-03-14 00:26:16 +0100

    Add more errors

--- src/error.rs
+++ src/error.rs
@@ -11,4 +11,14 @@ pub enum GeometryError {
     NoSphereAtIndex(usize, usize),
     #[error("The ray from point {0:?} to point {1:?} doesn't go through the point {1:?} (uh ?)")]
     RayBetweenPointsDoesNotHitPoint(Point, Point),
+    #[error("Source is not visible from point: {0}")]
+    SourceNotVisibleFromPoint(String),
+    #[error("Cannot create a unit vector or vector of norme 0")]
+    VectorHasNormeZero,
+}
+
+#[derive(Error, Debug)]
+pub enum ColorError {
+    #[error("Diffusion value should be a float coefficient between 0 and 1, got : {0}")]
+    DiffusionCoefficientOOB(String),
 }
