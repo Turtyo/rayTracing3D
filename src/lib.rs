c3fb055f2ae7ac9bb92401e5c28e77069960a614
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-03-14 23:32:08 +0100

    Should PartialEq based on float_cmp be used for f64 ?
    This might be counter-productive and most methods already implement a proper partialEq in the codebase
    + it's not always coordinates based, like points being distance based

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
 mod error;
 pub mod geometry;
 pub mod optic;
+
+// use float_cmp;
+
+// impl PartialEq for f64 {
+//     fn eq(&self, other: &Self) -> bool {
+
+//     }
+// }
+// * should use a wrapper with a deref on f64 to get all methods on f64 but that means changing all f64 references in the codebase
+// * this would allow to not have to implement PartialEq with float_cmp for each struct that uses f64
