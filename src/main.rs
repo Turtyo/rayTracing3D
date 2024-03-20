87f6209c102584ade112db50720ea0246740b9ed
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-03-20 19:39:17 +0100

    Need to setup the scene with some spheres and material to see if everything is working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use ray_tracing_3d::{geometry, object, optic, ray_trace_image};
+
 fn main() {
-    println!("Hello there!");
-    println!("General Kenobi");
+    let export_path = "ray_traced_images/some_spheres.png";
+    // * need to define the objects in the scene
+    // let sphere_center_1 = ;
+    // let sphere_center_2 = ;
+    // let sphere_center_3 = ;
+    // let sphere_center_4 = ;
+    // let sphere_center_5 = ;
+    // let sphere_center_6 = ;
+    // let object_1 = object::Object{shape: geometry::shape::Sphere::new_from_radius(center, radius)};
+    // ray_trace_image(10, 2, objects, export_path);
 }
