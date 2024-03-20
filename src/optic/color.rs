f4b7f0ad7b3d7ee3fb3f5e047c3e8d971473fffa
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-03-20 19:36:59 +0100

    Deprecate diffused color (not useful with the cos weighted distribution)

--- src/optic/color.rs
+++ src/optic/color.rs
@@ -148,90 +148,25 @@ impl Mul<&DiffusionCoefficient> for &Color {
     }
 }
 
-pub fn diffused_color(
-    source_color: Color,
-    object_diffusion_coefficient: DiffusionCoefficient,
-    source_ray: Ray,
-    surface_normal_vector: Vector,
-) -> Result<Color, RayTracingError> {
-    // ! start by checking if source is visible from the point
-    // since we only have spheres for now, this is done by checking the scalar product normal.ray is <= 0
-    // source is above the horizon if normal.(ray from surface to source) >= 0, since we have the ray from source to surface it's the opposite
-    let vector_from_surface_to_source = -1. * &source_ray.direction.to_vector();
-    let normal_ray_scalar_prod =
-        surface_normal_vector.scalar_product(&vector_from_surface_to_source);
-    if normal_ray_scalar_prod <= 0. {
-        Err(RayTracingError::SourceNotVisibleFromPoint(format!("Source has ray : {0:?} | Surface normal vector is : {1:?} | Their scalar product is {2}", source_ray, surface_normal_vector, normal_ray_scalar_prod)))
-    } else {
-        let angle = vector_from_surface_to_source.angle_with(&surface_normal_vector);
-        Ok(&(&source_color * &object_diffusion_coefficient) * angle.cos()) // change this by using a emisphere cos weighted distribution for rays directions
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::geometry::point::Point;
-
-    use super::*;
-
-    #[test]
-    fn test_diffused_color_black() -> Result<(), Box<dyn std::error::Error>> {
-        let source_color = BLACK;
-        let object_diffusion_coefficient = DiffusionCoefficient::new(1., 1., 1.)?;
-
-        let source = Point::new(-10.4900536536, -7.8544458162, 2.3623341028);
-        let ray_destination = Point::new(-2.244677331, 2.7337430702, 0.);
-        let source_ray = Ray::new_from_points(&source, &ray_destination)?;
-
-        let surface_normal_vector =
-            Vector::new_from_coordinates(0.799209635245991, -2.759857617631104, 0.862815095680144)?;
-
-        let result_color = super::diffused_color(
-            source_color,
-            object_diffusion_coefficient,
-            source_ray,
-            surface_normal_vector,
-        )?;
-        let expected_color = BLACK;
-
-        assert_eq!(result_color, expected_color);
-
-        let result_color_2 = super::diffused_color(
-            WHITE,
-            BLACK.to_diffusion_coefficient()?,
-            source_ray,
-            surface_normal_vector,
-        )?;
-        assert_eq!(result_color_2, expected_color);
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_diffused_color_blue() -> Result<(), Box<dyn std::error::Error>> {
-        let source = Point::new(-10.4900536536, -7.8544458162, 2.3623341028);
-        let ray_destination = Point::new(-2.244677331, 2.7337430702, 0.);
-        let source_ray = Ray::new_from_points(&source, &ray_destination)?;
-
-        let object_diffusion_coefficient = DiffusionCoefficient::new(0., 0., 0.5)?;
-        let source_color = Color::new(100, 240, u8::MAX);
-
-        let surface_normal_vector =
-            Vector::new_from_coordinates(0.799209635245991, -2.759857617631104, 0.862815095680144)?;
-
-        let result_color = super::diffused_color(
-            source_color,
-            object_diffusion_coefficient,
-            source_ray,
-            surface_normal_vector,
-        )?;
-
-        let b_expected = ((u8::MAX as f64) * 0.5 * 52.879169845223565_f64.to_radians().cos()) as u8;
-
-        let expected_color = Color::new(0, 0, b_expected);
-
-        assert_eq!(result_color, expected_color);
-
-        Ok(())
-    }
-}
+// ! deprecated since we use a cos weighted distribution and random bounces instead of searching for all light sources
+// * indeed, searching for light sources in the scene works if using point lights but does not work for area lights (as that would be an infinity of points)
+// pub fn diffused_color(
+//     source_color: Color,
+//     object_diffusion_coefficient: DiffusionCoefficient,
+//     source_ray: Ray,
+//     surface_normal_vector: Vector,
+// ) -> Result<Color, RayTracingError> {
+//     // ! start by checking if source is visible from the point
+//     // since we only have spheres for now, this is done by checking the scalar product normal.ray is <= 0
+//     // source is above the horizon if normal.(ray from surface to source) >= 0, since we have the ray from source to surface it's the opposite
+//     let vector_from_surface_to_source = -1. * &source_ray.direction.to_vector();
+//     let normal_ray_scalar_prod =
+//         surface_normal_vector.scalar_product(&vector_from_surface_to_source);
+//     if normal_ray_scalar_prod <= 0. {
+//         Err(RayTracingError::SourceNotVisibleFromPoint(format!("Source has ray : {0:?} | Surface normal vector is : {1:?} | Their scalar product is {2}", source_ray, surface_normal_vector, normal_ray_scalar_prod)))
+//     } else {
+//         // we use a emisphere cos weighted distribution for rays directions, meaning we don't correct the amount of light received here to follow Lambert Reflectance law
+//         // this correction is done by the cos weighted distribution
+//         Ok(&source_color * &object_diffusion_coefficient)
+//     }
+// }
