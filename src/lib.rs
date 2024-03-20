5b12a5ace5d473c76c354a4c58c13144c429283a
Turtyo <49365680+Turtyo@users.noreply.github.com>
2024-03-20 19:38:48 +0100

    function to call from outside, creates the grid, makes the image, exports the image

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,16 @@ pub mod optic;
 // }
 // * should use a wrapper with a deref on f64 to get all methods on f64 but that means changing all f64 references in the codebase
 // * this would allow to not have to implement PartialEq with float_cmp for each struct that uses f64
+
+pub fn ray_trace_image(
+    number_of_points_per_pixel: usize,
+    number_of_bounces: u64,
+    objects: &Vec<&object::Object>,
+    export_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut grid = optic::image::Grid::default();
+    grid.make_image(number_of_points_per_pixel, number_of_bounces, objects)?;
+    grid.export_image(export_path)?;
+
+    Ok(())
+}
